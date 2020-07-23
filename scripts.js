8f47f0580cd96ad52b08d1db0a30315ef9df22f7
dushyant0rawat <68513612+dushyant0rawat@users.noreply.github.com>
2020-07-22 21:46:22 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+async function FetchImg(url) {
+  try {
+    const response = await fetch(url);
+    const img = await response.blob();
+    document.getElementById("main").innerHTML = img;
+  }
+  catch (err) {
+    console.log('fetch failed', err);
+  }
+}
+FetchImg("cc-tree.jpg");
